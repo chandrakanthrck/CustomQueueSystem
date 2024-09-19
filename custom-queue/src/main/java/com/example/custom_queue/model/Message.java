1d6f0be49fded0faced5cd75176440a08b763b20
chandrakanthrck <chandrakanthrajesh@Chandrakanths-MacBook-Pro-2.local>
2024-09-19 16:34:16 -0700

    Add model Message

--- custom-queue/src/main/java/com/example/custom_queue/model/Message.java
+++ custom-queue/src/main/java/com/example/custom_queue/model/Message.java
@@ -1,12 +1,23 @@
 package com.example.custom_queue.model;
 
-public class Message<T> {
+import java.io.Serializable;
+import java.util.UUID;
+
+public class Message<T> implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private final String id;
     private final T content;
     private final long timestamp;
 
     public Message(T content) {
+        this.id = UUID.randomUUID().toString(); // Unique ID for each message
         this.content = content;
         this.timestamp = System.currentTimeMillis();
+    }
+
+    public String getId() {
+        return id;
     }
 
     public T getContent() {
@@ -15,5 +26,14 @@ public class Message<T> {
 
     public long getTimestamp() {
         return timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id='" + id + '\'' +
+                ", content=" + content +
+                ", timestamp=" + timestamp +
+                '}';
     }
 }
