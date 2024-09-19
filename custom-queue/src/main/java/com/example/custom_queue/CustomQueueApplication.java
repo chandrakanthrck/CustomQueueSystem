72f0ca5bbb6b1b857ab4e4e3f7f8fca992574eed
chandrakanthrck <chandrakanthrajesh@Chandrakanths-MacBook-Pro-2.local>
2024-09-19 13:21:47 -0700

    Spring Initialzr

---
+++ custom-queue/src/main/java/com/example/custom_queue/CustomQueueApplication.java
@@ -0,0 +1,13 @@
+package com.example.custom_queue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomQueueApplication.class, args);
+	}
+
+}
